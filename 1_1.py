0df777a210ef38b0fd4ff960473a560d66a3f74d
dazvid <david.mcleod@gmail.com>
2017-04-01 00:17:58 +1100

    Cleaned 1_1

--- 1_1.py
+++ 1_1.py
@@ -12,12 +12,11 @@ The string:
     SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 """
 
-import binascii
+from binascii import unhexlify, b2a_base64
 
 def hex_to_b64(s):
-    byte_string = binascii.unhexlify(s)
-    string_b64 = binascii.b2a_base64(byte_string).decode('utf-8')
-    return string_b64
+    byte_string = unhexlify(s)
+    return b2a_base64(byte_string).decode('utf-8')
 
 hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 
